ce2d3c411682aeab2a526883153a9d9146303bc2
Pablo Roda Romero <proda@gamma20.aulas.gsyc.urjc.es>
2016-09-19 15:22:25 +0200

    Calcoo.py terminado

--- calcoo.py
+++ calcoo.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
 import sys
 
 class Calculadora():
 	def suma(self, op1, op2):
 		return op1 + op2
+	def resta(self, op1, op2):
+		return op1 - op2
 
 if __name__ == "__main__":
 	try:
@@ -16,6 +19,8 @@ if __name__ == "__main__":
 
 	if sys.argv[2] == "suma":
 		result = calc.suma(operando1, operando2)
+	elif sys.argv[2] == "resta":
+		result = calc.resta(operando1, operando2)
 	else:
 		sys.exit('Operación sólo puede ser sumar, restar')
 
