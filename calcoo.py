5737e54790a6fa2d9caba90e221e62444d9b8b8f
Pablo Roda Romero <proda@alpha08.aulas.gsyc.urjc.es>
2016-09-19 13:04:02 +0200

    calcoo.py

---
+++ calcoo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+
+class Calculadora():
+	def suma(self, op1, op2):
+		return op1 + op2
+
+if __name__ == "__main__":
+	try:
+		operando1 = int(sys.argv[1])
+		operando2 = int(sys.argv[3])
+		calc = Calculadora()
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
+
+	if sys.argv[2] == "suma":
+		result = calc.suma(operando1, operando2)
+	else:
+		sys.exit('Operación sólo puede ser sumar, restar')
+
+	print(result)
