59e3fb9786ff052cf4a7e1ab05afc2c89c83228e
Pablo Roda Romero <proda@gamma36.aulas.gsyc.urjc.es>
2016-09-26 15:49:51 +0200

    corregido pep8

--- calcplus.py
+++ calcplus.py
@@ -9,29 +9,29 @@ import calcoohija
 calculadora = calcoohija.Calcoohija()
 
 if __name__ == "__main__":
-	try:
-		fichero = open('Operaciones', 'r')
-	except:
-		sys.exit("Error: file not found")
+    try:
+        fichero = open('Operaciones', 'r')
+    except:
+        sys.exit("Error: file not found")
 
-	for linea in lineas:
-		elementos = linea.split(',')
-		operador = elementos[0]
-		operando1 = int(elementos[1])
-		operandos = elementos[2:]
-		result = operando1
+    for linea in lineas:
+        elementos = linea.split(',')
+        operador = elementos[0]
+        operando1 = int(elementos[1])
+        operandos = elementos[2:]
+        result = operando1
 
-		if operador == "suma":
-			for operando in operandos:
-				result = calculadora.suma(result, int(operando))
-		if operador == "resta":
-			for operando in operandos:
-				result = calculadora.resta(result, int(operando))
-		if operador == "multiplica":
-			for operando in operandos:
-				result = calculadora.multiplicacion(result, int(operando))
-		if operador == "divide":
-			for operando in operandos:
-				result = calculadora.division(result, int(operando))
-			
-		print(result)
+        if operador == "suma":
+            for operando in operandos:
+                result = calculadora.suma(result, int(operando))
+        if operador == "resta":
+            for operando in operandos:
+                result = calculadora.resta(result, int(operando))
+        if operador == "multiplica":
+            for operando in operandos:
+                result = calculadora.multiplicacion(result, int(operando))
+        if operador == "divide":
+            for operando in operandos:
+                result = calculadora.division(result, int(operando))
+
+        print(result)
