fc7a071d94141ce9bd4b6a853c7f4330925c07f9
Pablo Roda Romero <proda@gamma36.aulas.gsyc.urjc.es>
2016-09-26 15:22:03 +0200

    Terminado

---
+++ calcplusplus.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+import calcoo
+import calcoohija
+
+calculadora = calcoohija.Calcoohija()
+
+argumento = sys.argv[1]
+
+if __name__ == "__main__":
+	try:
+		fichero = open(argumento, 'r')
+	except:
+		sys.exit("Error: file not found")
+
+	with open(argumento) as operaciones:
+		lineas = csv.reader(operaciones)
+
+		for linea in lineas:
+			operador = linea[0]
+			operando1 = int(linea[1])
+			operandos = linea[2:]
+			result = operando1
+
+			if operador == "suma":
+				for operando in operandos:
+					result = calculadora.suma(result, int(operando))
+			if operador == "resta":
+				for operando in operandos:
+					result = calculadora.resta(result, int(operando))
+			if operador == "multiplica":
+				for operando in operandos:
+					result = calculadora.multiplicacion(result, int(operando))
+			if operador == "divide":
+				for operando in operandos:
+					result = calculadora.division(result, int(operando))
+			
+			print(result)
